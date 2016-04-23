0733a44ea11b58a458f8182f31e37fd30ea25607
Rahul Gopinath <rahul@gopinath.org>
2016-04-23 13:11:16 -0700

    license

--- Makefile
+++ Makefile
@@ -1,10 +1,18 @@
-P=programs/wc.c
-gen: bin/descmutants
-	./bin/gen.sh $(P)
 
 bin/descmutants: src/descmutants.pl
 	mkdir -p bin
 	swipl --goal=describe_mutants --stand_alone=true --local=32768 -o ./bin/descmutants -c src/descmutants.pl
+
+P=wc.c
+G=build/wc.c/mutants.txt
+gen: bin/descmutants $(G)
+	./bin/gen.sh programs/$(P)
+
+
+build/%/mutants.txt: bin/descmutants
+	mkdir -p build/$*
+	./bin/descmutants < programs/$* > $@
+
 
 clobber:
 	rm -f bin/descmutants
--- bin/gen.sh
+++ bin/gen.sh
@@ -1,26 +1,5 @@
-#!/bin/sh
-# generateMutants.sh -- generate mutants of file
-# Copyright (c) 2005 Jamie Andrews (andrews [[at]] csd.uwo.ca)
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-#
-# Changes:
-#   None (first public release).
-#   08/22/2011 by Chaoqiang Zhang
-#   Randomly generate some mutants using user specified number.
-
+#!/usr/bin/env bash
+# See LICENSE (GPL V2)
 if test $# = 1
 then
   SOURCE=$1
@@ -31,35 +10,28 @@ fi
 
 name=$(basename $SOURCE)
 myname=build/$name/
-mkdir -p $myname
 mkdir -p $myname/mutants
 
-DESCS=$myname/MutantDescs.txt
-echo Generating mutant descriptions into file ${DESCS}...
-# Assumes that executable "descmutants" has been made and is accessible.
-./bin/descmutants <$SOURCE >$DESCS
-Total=`wc -l < $DESCS`
-echo Number of mutant descriptions generated:  $Total
+DESCS=$myname/mutants.txt
+total_mutants=$(wc -l < $DESCS)
+echo Number of mutant descriptions generated:  $total_mutants
 
 echo Generating mutants of ${SOURCE}...
-Mutant=0
-# Starting with code 100001 because that makes it much easier for
-# shell scripts that are going to step through all mutants.
-MutantCode=100000
 
-for Mutant in $(seq 1 $Total)
+for mutated_line in $(seq 1 $total_mutants)
 do
-  mutated_line=$(sed -ne "$Mutant s/:.*//p" < $DESCS)
-  LineNoM1=`expr $mutated_line - 1`
-  LineNoP1=`expr $mutated_line + 1`
-  mymutant=build/$name/mutants/${Mutant}_${name}
+  mymutant=build/$name/mutants/${mutated_line}_${name}
 
-  echo Generating mutant source file $mymutant with mutated line $mutated_line
-  sed -ne "1,$LineNoM1 p" < $SOURCE >$mymutant
-  sed -ne "$Mutant s/^[^:]*://p" < $DESCS >>$mymutant
-  sed -ne "$LineNoP1,$ p" < $SOURCE >>$mymutant
+  mut_line_no=$(sed -ne "$mutated_line s/:.*//p" < $DESCS)
+  before_mutant=$(($mut_line_no - 1))
+  after_mutant=$(($mut_line_no + 1))
+
+  echo Generating mutant source file $mymutant with mutated line $mut_line_no
+  sed -ne "1,$before_mutant p" < $SOURCE >$mymutant
+  sed -ne "$mutated_line s/^[^:]*://p" < $DESCS >>$mymutant
+  sed -ne "$after_mutant,$ p" < $SOURCE >>$mymutant
 done
 
-echo Number of mutants generated in `pwd`: $Mutant
+echo Number of mutants generated in $PWD: $mutated_line
 
 exit 0
